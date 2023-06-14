1627c6b03d3060e70a9a42c8c9372c619b7b5168
alexyesilyurt <yesilyurtalexander@gmail.com>
2023-06-14 17:46:27 -0400

    first commit

---
+++ javascript.js
@@ -0,0 +1,57 @@
+let symbol = "X";
+let gameEnd = false;
+for (let i = 1; i <= 9; i++) {
+    document.getElementById(i.toString()).addEventListener("click", function () {
+        if (this.innerHTML === "" && !gameEnd) {
+            this.innerHTML = symbol;
+            this.classList.add(symbol.toLowerCase());
+
+            checkWin();
+
+            if (symbol === "X") {
+                symbol = "O";
+            }
+            else {
+                symbol = "X";
+            }
+        }
+    });
+}
+let winBoard = [
+    [1, 2, 3], [4, 5, 6],
+    [7, 8, 9], [1, 4, 7],
+    [2, 5, 8], [3, 6, 9],
+    [1, 5, 9], [3, 5, 7]
+];
+function checkWin() {
+    for (let i = 0; i < winBoard.length; i++) {
+        if (
+            document.getElementById(winBoard[i][0]).innerHTML === symbol &&
+            document.getElementById(winBoard[i][1]).innerHTML === symbol &&
+            document.getElementById(winBoard[i][2]).innerHTML === symbol
+        ) {
+            document.getElementById(winBoard[i][0]).classList.add("win");
+            document.getElementById(winBoard[i][1]).classList.add("win");
+            document.getElementById(winBoard[i][2]).classList.add("win");
+
+            gameEnd = true;
+
+            setTimeout(function () {
+                alert(symbol + " wins!");
+            }, 250);
+        }
+    }
+}
+document.getElementById("reset").addEventListener(
+    "click",
+    function () {
+        for (let i = 1; i <= 9; i++) {
+            document.getElementById(i.toString()).innerHTML = "";
+            document.getElementById(i.toString()).classList.remove("x");
+            document.getElementById(i.toString()).classList.remove("o");
+            document.getElementById(i.toString()).classList.remove("win");
+            symbol = "X";
+            gameEnd = false;
+        }
+    }
+);
